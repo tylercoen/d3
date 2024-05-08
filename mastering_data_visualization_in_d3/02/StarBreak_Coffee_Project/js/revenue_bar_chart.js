c50d3afce5e33774ab0b423fcfe153e7d707316c
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2024-05-08 19:00:27 -0400

    Add files via upload
    Added bar charts and x axis. Need to adjust y axis and labels.

--- mastering_data_visualization_in_d3/02/StarBreak_Coffee_Project/js/revenue_bar_chart.js
+++ mastering_data_visualization_in_d3/02/StarBreak_Coffee_Project/js/revenue_bar_chart.js
@@ -6,15 +6,15 @@
 
 // Add an svg element, group, and margins
 
-const MARGIN = { LEFT: 50, RIGHT: 50, TOP: 50, BOTTOM: 50 };
-const WIDTH = 600 - MARGIN.LEFT - MARGIN.LEFT;
-const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM;
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 130 };
+const WIDTH = 800 - MARGIN.LEFT - MARGIN.LEFT;
+const HEIGHT = 600 - MARGIN.TOP - MARGIN.BOTTOM;
 
 const svg = d3
   .select("#chart-area")
   .append("svg")
-  .attr("width", WIDTH)
-  .attr("height", HEIGHT);
+  .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+  .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM);
 
 // Use to check svg area
 /*svg
@@ -23,11 +23,8 @@ const svg = d3
   .attr("width", "100%")
   .attr("fill", "red");*/
 
-const g = svg
-  .append("g")
-  .attr("transform", `translate(${MARGIN.LEFT},${MARGIN.TOP})`)
-  .style("fill", "grey");
-
+const g = svg.append("g").style("fill", "grey");
+//  .attr("transform", `translate(${MARGIN.LEFT},${MARGIN.TOP})`)
 // Load the data
 d3.csv("./data/revenues.csv").then((data) => {
   data.forEach((d) => {
@@ -40,16 +37,34 @@ d3.csv("./data/revenues.csv").then((data) => {
   const x = d3
     .scaleBand()
     .domain(data.map((d) => d.month))
-    .range([0, WIDTH]);
+    .range([0, WIDTH])
+    .paddingInner(0.3)
+    .paddingOuter(0.2);
 
   const y = d3
     .scaleLinear()
     .domain([0, d3.max(data, (d) => d.revenue)])
     .range([HEIGHT, 0]);
+
+  // add rectangles for each month
+
+  const rects = g.selectAll("rect").data(data);
+  //  scale the rectangles and move to bottom
+
+  rects
+    .enter()
+    .append("rect")
+    .attr("y", (d) => y(d.revenue))
+    .attr("x", (d) => x(d.month))
+    .attr("width", x.bandwidth)
+    .attr("height", (d) => HEIGHT - y(d.revenue));
+
+  // add axes and labels
+  const xAxisScale = d3.axisBottom(x);
+
+  g.append("g").call(xAxisScale).attr("transform", `translate(0, ${HEIGHT})`);
+
+  const yAxisScale = d3.axisLeft(y);
+
+  g.append("g").call(yAxisScale);
 });
-
-// add rectangles for each month
-
-//  scale the rectangles and move to bottom
-
-// add axes and labels
