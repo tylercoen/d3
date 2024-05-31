a45c10f81435925033bf1df71d11fda6f7a4361a
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2024-05-31 13:48:47 -0400

    Add files via upload
    Finished the visualization to plot by year.

--- mastering_data_visualization_in_d3/gapminder_clone/js/main.js
+++ mastering_data_visualization_in_d3/gapminder_clone/js/main.js
@@ -18,10 +18,124 @@ const g = svg
   .append("g")
   .attr("transform", `translate(${MARGIN.LEFT},${MARGIN.TOP})`);
 
-const x = d3.scaleLog().range([0, WIDTH]);
+async function fetchData() {
+  try {
+    const data = await d3.json("data/data.json");
+    return data;
+  } catch (error) {
+    console.log("Error fetching data: ", error);
+  }
+}
+fetchData().then((data) => {
+  if (data) {
+    const formattedData = data.map((year) => {
+      return year["countries"]
+        .filter((country) => {
+          const dataExists = country.income && country.life_exp;
+          return dataExists;
+        })
+        .map((country) => {
+          country.income = Number(country.income);
+          country.life_exp = Number(country.life_exp);
+          country.population = Number(country.population);
+          return country;
+        });
+    });
+    //console.log(formattedData[0]);
+    //console.log(formattedData[6]);
+
+    const incomeExtent = d3.extent(formattedData.flat(), (d) => d.income);
+
+    const x = d3.scaleLog().domain(incomeExtent).range([0, WIDTH]);
+    const xAxisScale = d3
+      .axisBottom(x)
+      .tickValues([400, 4000, 40000])
+      .tickFormat(d3.format(",d"));
+
+    g.append("g").call(xAxisScale).attr("transform", `translate(0, ${HEIGHT})`);
+
+    const y = d3.scaleLinear().domain([0, 90]).range([HEIGHT, 0]);
+
+    const yAxisScale = d3.axisLeft(y);
+
+    g.append("g").call(yAxisScale);
+
+    // Below are going to
+
+    const radiusScale = d3
+      .scaleSqrt()
+      .domain([0, d3.max(formattedData[0], (d) => d.population)])
+      .range([0, 40]);
+
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10); //
+
+    let index = 0;
+
+    d3.interval(() => {
+      // Increment the index and wrap around if it exceeds the length of the data
+      index = (index + 1) % formattedData.length;
+
+      // Determine the slice of data to use based on the index
+      const newData = formattedData[index];
+
+      // Update with the new data
+      update(newData);
+    }, 1000);
+
+    function update(data) {
+      const t = d3.transition().duration(750);
+      //console.log(data[0]);
+
+      // JOIN
+
+      const circles = g.selectAll("circle");
+
+      // EXIT
+
+      circles.exit().transition(t).attr("r", 0).remove();
+
+      // ENTER
+      circles
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("fill-opacity", 0.6)
+        .merge(circles)
+        .transition(t)
+        .attr("r", (d) => radiusScale(d.population))
+        .attr("fill", (d) => colorScale(d.country))
+        .attr("cx", (d) => x(d.income))
+        .attr("cy", (d) => y(d.life_exp));
+    }
+  }
+});
+
+/*
+ *    main.js
+ *    Mastering Data Visualization with D3.js
+ *    Project 2 - Gapminder Clone
+ 
+
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 130 };
+const WIDTH = 1000 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 600 - MARGIN.TOP - MARGIN.BOTTOM;
+
+const svg = d3
+  .select("#chart-area")
+  .append("svg")
+  .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+  .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM);
+
+const g = svg
+  .append("g")
+  .attr("transform", `translate(${MARGIN.LEFT},${MARGIN.TOP})`);
+
+const incomeExtent = d3.extent(formattedData.flat(), (d) => d.income);
+
+const x = d3.scaleLog().domain(incomeExtent).range([0, WIDTH]);
 const xAxisGroup = g.append("g").attr("transform", `translate(0, ${HEIGHT})`);
 
-const y = d3.scaleLinear().range([HEIGHT, 0]);
+const y = d3.scaleLinear().domain([0, 90]).range([HEIGHT, 0]);
 const yAxisGroup = g.append("g");
 //for the transition
 d3.json("data/data.json").then((data) => {
@@ -38,6 +152,7 @@ d3.json("data/data.json").then((data) => {
         return country;
       });
   });
+
   // create interval I need to figure out the flag values I'm guessing it should be year
   d3.interval(() => {
     flag = !flag;
@@ -51,9 +166,6 @@ d3.json("data/data.json").then((data) => {
 function update(data) {
   const t = d3.transition().duration(750);
 
-  const incomeExtent = d3.extent(data.flat(), (d) => d.income);
-  x.domain(incomeExtent);
-  y.domain([0, 90]);
   const xAxisCall = d3
     .axisBottom(x)
     .tickValues([400, 4000, 40000])
@@ -76,7 +188,7 @@ function update(data) {
 
   // EXIT
 
-  circles.exit().transition(t).attr("cy", y(0)).remove();
+  circles.exit().transition(t).attr("r", 0).remove();
 
   // ENTER
   circles
@@ -90,3 +202,4 @@ function update(data) {
     .attr("cx", (d) => x(d.income))
     .attr("cy", (d) => y(d.life_exp));
 }
+*/
