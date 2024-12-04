fc339e7dc798f14bb3530be00cc373cec206a1cc
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2024-12-04 00:00:15 +0000

    I got the treemap to display but only one at a time.

--- treemap/js/main.js
+++ treemap/js/main.js
@@ -8,19 +8,99 @@ const containerWidth = 1200;
 const containerHeight = 650;
 const padding = 25;
 const mapWidth = containerWidth - 2 * padding;
-const maphHeight = containerHeight - 2 * padding;
+const mapHeight = containerHeight - 2 * padding;
+const margin = 100;
+var svg = d3
+  .select("#treemap")
+  .append("svg")
+  .attr("width", mapWidth + 2 * margin)
+  .attr("height", mapHeight + 2 * margin)
+  .append("g")
+  .attr("transform", "translate(" + margin + "," + margin + ")");
 
 Promise.all([d3.json(VIDEO_GAME_SALES_URL)]).then(([data]) => {
   var videoGameSalesData = data.children;
   console.log(videoGameSalesData);
-  console.log(videoGameSalesData[0].children[0].value);
+  ///console.log(videoGameSalesData[0].children[0].value);
 
-  var root = d3.hierarchy(videoGameSalesData[1]).sum((d) => d.value);
+  const root = d3.hierarchy(videoGameSalesData).sum((d) => d.value);
 
   //create svg
-  var svg = d3
-    .select("#treemap")
-    .append("svg")
-    .attr("width", containerWidth)
-    .attr("height", containerHeight);
+  d3
+    .treemap()
+    .size([mapWidth, mapHeight])
+    .paddingTop(28)
+    .paddingRight(7)
+    .paddingInner(3)(
+    // Padding between each rectangle
+    //.paddingOuter(6)
+    //.padding(20)
+    root
+  );
+  ///////////////////////////////////////////////////////////////
+  /*
+  // Create a group for each node
+  const cells = svg
+    .selectAll("g")
+    .data(root.leaves())
+    .enter()
+    .append("g")
+    .attr("transform", (d) => `translate(${d.x0},${d.y0})`);
+
+  // Create rectangles for each node
+  cells
+    .append("rect")
+    .attr("width", (d) => d.x1 - d.x0)
+    .attr("height", (d) => d.y1 - d.y0)
+    .style("fill", "steelblue");
+
+  // Add text labels to each node
+  cells
+    .append("text")
+    .attr("x", 3)
+    .attr("y", 13)
+    .text((d) => d.data.name)
+    .style("font-size", "10px")
+    .style("fill", "white");
+    */
+  ///////////////////////////////////////////////
+  // use this information to add rectangles:
+  svg
+    .selectAll("rect")
+    .data(root.leaves())
+    .enter()
+    .append("rect")
+    .attr("x", (d) => d.x0)
+    .attr("y", (d) => d.y0)
+    .attr("width", (d) => d.x1 - d.x0)
+    .attr("height", (d) => d.y1 - d.y0)
+    .style("stroke", "black")
+    //.style("fill", (d) => color(d.parent.data.name)) remove the use of color and create a function that fills by category
+    .style("opacity", (d) => opacity(d.data.value));
+
+  const colorScale = d3
+    .scaleOrdinal()
+    .domain([
+      "2600",
+      "Wii",
+      "NES",
+      "GB",
+      "DS",
+      "X360",
+      "PS3",
+      "PS2",
+      "SNES",
+      "GBA",
+      "PS4",
+      "3DS",
+      "N64",
+      "PS",
+      "XB",
+      "PC",
+      "PSP",
+      "XOne",
+    ])
+    .range(d3.schemeCategory10);
+
+  var opacity = d3.scaleLinear().domain([10, 30]).range([0.5, 1]);
 });
